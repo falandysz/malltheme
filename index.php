9b292a6a08743769c2f303641dc8bb3d278b5247
Grzegorz Falandysz <grzegorz.falandysz@pds.sos.pl>
2015-03-28 20:49:00 +0100

    draft of main page added

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Grzegorz Falandysz
+<?php get_header(); ?>
+<div id="content" class="row">
+	<div id="zawartosc" class="col-lg-10">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+		the_content();
+		endwhile; else: ?>
+		<p>Przepraszamy, w tym miejscu nie ma żadnych artykułów.</p>
+		<?php endif; ?>
+	</div>
+	<?php include ('sidebar-right.php');?>
+</div>
+<?php get_footer(); ?>
